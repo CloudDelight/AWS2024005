f1d5449e43254cb505fc63443608f65238b31808
CloudDelight <clouddelight4knowledge@gmail.com>
2025-01-14 12:48:47 +0530

    linux commnads added

---
+++ linux.sh
@@ -0,0 +1,41 @@
+#vi - Visual editor
+#cat - check the content of the file
+#ls - list of files
+#mv - move from source to destination
+#rm - remove
+#rf -rf --> permanent delete
+#df -h --> disk space
+#fdisk -l --> volumes
+#clear - clear
+#top --> cpu
+#free -m --> memory
+#grep --> selective search
+#wc - word count
+#date - date
+#pwd - Present working directory
+#ping,tcpdump,telnet - network commands
+#chmod - permission
+#chown - ownership
+
+read - 4
+write - 2
+execute - 1
+
+rwxrwxrwx - 777
+r-xrwxr-- - 574
+#useradd - adding user
+#groupadd - group creation
+
+
+Interview question
+1. When you add user - useradd command - what are the files at the ackgroud gets modified?
+
+a. /etc/user - folder gets created
+b. /etc/grooups - group will be craeted
+c. /etc/passwd - modified
+
+#head 
+#tail -100f log - listing files content
+#wget - download content
+
+
